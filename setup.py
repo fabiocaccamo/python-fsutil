7912a17fc4aea6f00afbfb85682e85293fc7d747
Fabio Caccamo <fabio.caccamo@gmail.com>
2020-10-27 17:26:12 +0100

    Added setup files.

---
+++ setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+import os, sys
+
+exec(open('fsutil/metadata.py').read())
+
+github_url = 'https://github.com/fabiocaccamo'
+package_name = 'python-fsutil'
+package_url = '{}/{}'.format(github_url, package_name)
+package_path = os.path.abspath(os.path.dirname(__file__))
+long_description_file_path = os.path.join(package_path, 'README.md')
+long_description_content_type = 'text/markdown'
+long_description = ''
+try:
+    long_description_file_options = {} if sys.version_info[0] < 3 else { 'encoding':'utf-8' }
+    with open(long_description_file_path, 'r', **long_description_file_options) as f:
+        long_description = f.read()
+except IOError:
+    pass
+
+setup(
+    name=package_name,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    include_package_data=True,
+    version=__version__,
+    description=__description__,
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    author=__author__,
+    author_email=__email__,
+    url=package_url,
+    download_url='{}/archive/{}.tar.gz'.format(package_url, __version__),
+    keywords=[
+        'python', 'file', 'system', 'util', 'utils', 'utility', 'utilities',
+        'dir', 'directory', 'path', 'fs', 'os', 'shutil', 'glob',
+    ],
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: MacOS X',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Desktop Environment :: File Managers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Filesystems',
+        'Topic :: Utilities',
+    ],
+    license=__license__,
+    test_suite='tests'
+)
