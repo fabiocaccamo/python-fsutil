68efd75dfb46d83a83b9cb031b56907b029d95a9
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-01-12 02:15:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,25 +5,30 @@ from setuptools import find_packages, setup
 
 import os, sys
 
-exec(open('fsutil/metadata.py').read())
+exec(open("fsutil/metadata.py").read())
 
-github_url = 'https://github.com/fabiocaccamo'
-package_name = 'python-fsutil'
-package_url = '{}/{}'.format(github_url, package_name)
+github_url = "https://github.com/fabiocaccamo"
+sponsor_url = "https://github.com/sponsors/fabiocaccamo/"
+twitter_url = "https://twitter.com/fabiocaccamo"
+package_name = "python-fsutil"
+package_url = "{}/{}".format(github_url, package_name)
+package_issues_url = "{}/issues".format(github_url)
 package_path = os.path.abspath(os.path.dirname(__file__))
-long_description_file_path = os.path.join(package_path, 'README.md')
-long_description_content_type = 'text/markdown'
-long_description = ''
+long_description_file_path = os.path.join(package_path, "README.md")
+long_description_content_type = "text/markdown"
+long_description = ""
 try:
-    long_description_file_options = {} if sys.version_info[0] < 3 else { 'encoding':'utf-8' }
-    with open(long_description_file_path, 'r', **long_description_file_options) as f:
+    long_description_file_options = (
+        {} if sys.version_info[0] < 3 else {"encoding": "utf-8"}
+    )
+    with open(long_description_file_path, "r", **long_description_file_options) as f:
         long_description = f.read()
 except IOError:
     pass
 
 setup(
     name=package_name,
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     include_package_data=True,
     version=__version__,
     description=__description__,
@@ -32,38 +37,56 @@ setup(
     author=__author__,
     author_email=__email__,
     url=package_url,
-    download_url='{}/archive/{}.tar.gz'.format(package_url, __version__),
+    download_url="{}/archive/{}.tar.gz".format(package_url, __version__),
+    project_urls={
+        "Documentation": package_url,
+        "Issues": package_issues_url,
+        "Funding": sponsor_url,
+        "Twitter": twitter_url,
+    },
     keywords=[
-        'python', 'file', 'system', 'util', 'utils', 'utility', 'utilities',
-        'dir', 'directory', 'path', 'fs', 'os', 'shutil', 'glob',
+        "python",
+        "file",
+        "system",
+        "util",
+        "utils",
+        "utility",
+        "utilities",
+        "dir",
+        "directory",
+        "path",
+        "fs",
+        "os",
+        "shutil",
+        "glob",
     ],
     install_requires=[
-        'requests',
+        "requests",
     ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: MacOS X',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Desktop Environment :: File Managers',
-        'Topic :: Software Development :: Build Tools',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Filesystems',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: MacOS X",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Desktop Environment :: File Managers",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Filesystems",
+        "Topic :: Utilities",
     ],
     license=__license__,
-    test_suite='tests'
+    test_suite="tests",
 )
