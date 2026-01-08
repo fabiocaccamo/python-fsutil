a35062c2202a2d0fe4734c736b5b4324fb7327f4
Fabio Caccamo <fabio.caccamo@gmail.com>
2026-01-08 23:36:01 +0100

    Normalize temp path to use OS specific separator.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ def temp_path():
     with tempfile.TemporaryDirectory() as temp_dir:
 
         def _temp_path(filepath=""):
-            return os.path.join(temp_dir, filepath)
+            # normalize path to use OS-specific separators
+            return os.path.normpath(os.path.join(temp_dir, filepath))
 
         yield _temp_path
